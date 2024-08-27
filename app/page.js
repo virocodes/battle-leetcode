06630b695c6ca72b97fb4793088819331ce7b733
Dharshan Bellan <dbellan1291@gmail.com>
2024-08-26 21:24:04 -0700

    basic nextjs app

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">Battle Leetcode</h1>
+    </div>
+    
+  );
+}
