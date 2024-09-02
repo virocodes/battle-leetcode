661658854b5dda35959ba532ac035bd4f0728c1d
Julianna Cardenas <julmcardenas@gmail.com>
2024-09-02 03:14:40 -0700

    added test cases section

--- app/battle/page.js
+++ app/battle/page.js
@@ -11,6 +11,7 @@ import { javascript } from "@codemirror/lang-javascript";
 
 import { twoSum } from '@/app/utils/problems/two-sum';
 import Image from 'next/image';
+import EditorFooter from '@/app/components/EditorFooter';
 
 function HealthBar({ label, health }) {
     return (
@@ -31,7 +32,8 @@ export default function Battle() {
     const [opponentHealth, setOpponentHealth] = useState(50);
 
     const [problem, setProblem] = useState(twoSum);
-    
+
+    const [activeTestCaseId, setActiveTestCaseId] = useState(0);
     const [code, setCode] = useState(problem.starterCode);
     const [theme, setTheme] = useState(githubLight);
 
@@ -62,6 +64,60 @@ export default function Battle() {
     const handleCodeChange = (newCode) => {
         setCode(newCode);
         localStorage.setItem('userCode', newCode);
+    };
+
+    const handleSubmit = async () => {
+        // if (!user) {
+        // 	toast.error("Please login to submit your code", {
+        // 		position: "top-center",
+        // 		autoClose: 3000,
+        // 		theme: "dark",
+        // 	});
+        // 	return;
+        // }
+        // try {
+        // 	userCode = userCode.slice(userCode.indexOf(problem.starterFunctionName));
+        // 	const cb = new Function(`return ${userCode}`)();
+        // 	const handler = problems[pid].handlerFunction;
+
+        // 	if (typeof handler === "function") {
+        // 		const success = handler(cb);
+        // 		if (success) {
+        // 			toast.success("Congrats! All tests passed!", {
+        // 				position: "top-center",
+        // 				autoClose: 3000,
+        // 				theme: "dark",
+        // 			});
+        // 			setSuccess(true);
+        // 			setTimeout(() => {
+        // 				setSuccess(false);
+        // 			}, 4000);
+
+        // 			const userRef = doc(firestore, "users", user.uid);
+        // 			await updateDoc(userRef, {
+        // 				solvedProblems: arrayUnion(pid),
+        // 			});
+        // 			setSolved(true);
+        // 		}
+        // 	}
+        // } catch (error) {
+        // 	console.log(error.message);
+        // 	if (
+        // 		error.message.startsWith("AssertionError [ERR_ASSERTION]: Expected values to be strictly deep-equal:")
+        // 	) {
+        // 		toast.error("Oops! One or more test cases failed", {
+        // 			position: "top-center",
+        // 			autoClose: 3000,
+        // 			theme: "dark",
+        // 		});
+        // 	} else {
+        // 		toast.error(error.message, {
+        // 			position: "top-center",
+        // 			autoClose: 3000,
+        // 			theme: "dark",
+        // 		});
+        // 	}
+        // }
     };
 
     return (
@@ -118,27 +174,65 @@ export default function Battle() {
             </div>
 
             {/* Right */}
-            <Split className='dark:bg-dark-layer-1 dark:text-white' direction="vertical" sizes={[70, 30]} minSize={100}>
+            <div className='flex flex-col '>
+                <Split className='flex-grow dark:bg-dark-layer-1 dark:text-white' direction="vertical" sizes={[70, 30]} minSize={100}>
+                    {/* Code Editor */}
+                    <div className="w-full p-4 overflow-auto">
+                        <h1 className="text-2xl font-bold mb-2">Code Editor</h1>
+                        <CodeMirror
+                            value={code}
+                            theme={theme}
+                            onChange={(editor, data, value) => {
+                                handleCodeChange(editor);
+                            }}
+                            extensions={[javascript()]}
+                        />
+                    </div>
 
+                    {/* Test Cases */}
+                    <div className='w-full px-5 overflow-auto'>
+                        {/* testcase heading */}
+                        <div className='flex h-10 items-center space-x-6'>
+                            <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                                <div className='text-sm font-medium leading-5 text-white'>Testcases</div>
+                                <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+                            </div>
+                        </div>
 
-                {/* Code Editor */}
-                <div className="w-full p-4 overflow-auto">
-                    <h1 className="text-2xl font-bold mb-2">Code Editor</h1>
-                    <CodeMirror
-                        value={code}
-                        theme={theme}
-                        onChange={(editor, data, value) => {
-                            handleCodeChange(editor);
-                        }}
-                        extensions={[javascript()]}
-                    />
-                </div>
+                        <div className='flex'>
+                            {problem.examples.map((example, index) => (
+                                <div
+                                    className='mr-2 items-start mt-2 '
+                                    key={example.id}
+                                    onClick={() => setActiveTestCaseId(index)}
+                                >
+                                    <div className='flex flex-wrap items-center gap-y-4'>
+                                        <div
+                                            className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+									`}
+                                        >
+                                            Case {index + 1}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
 
-                {/* Terminal */}
-                <div className="w-full p-4 overflow-auto">
-                    <h1 className="text-2xl font-bold mb-2">Terminal</h1>
-                </div>
-            </Split>
+                        <div className='font-semibold my-4'>
+                            <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                                {problem.examples[activeTestCaseId].inputText}
+                            </div>
+                            <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                                {problem.examples[activeTestCaseId].outputText}
+                            </div>
+                        </div>
+                    </div>
+                </Split>
+                <EditorFooter handleSubmit={handleSubmit} />
+            </div>
 
         </Split>
 
