9cd171d8aed8879ea39396cc678de8df7955760d
Julianna Cardenas <julmcardenas@gmail.com>
2024-09-04 16:10:30 -0700

    fixed test cases light mode

--- app/components/EditorFooter.js
+++ app/components/EditorFooter.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const EditorFooter = ({ handleSubmit }) => {
 	return (
-		<div className='flex bg-dark-layer-1  w-full'>
+		<div className='flex dark:bg-dark-layer-1 w-full'>
 			<div className='mx-5 my-[10px] flex justify-between w-full'>
 				<div className='mr-2 flex flex-1 flex-nowrap items-center space-x-4'>
-					<button className='px-3 py-1.5 font-medium items-center transition-all inline-flex bg-dark-fill-3 text-sm hover:bg-dark-fill-2 text-dark-label-2 rounded-lg pl-3 pr-2'>
+					<button className='px-3 py-1.5 font-medium items-center transition-all inline-flex dark:bg-dark-fill-3 bg-gray-200 text-sm hover:bg-dark-fill-2 dark:text-dark-label-2 rounded-lg pl-3 pr-2'>
 						Console
 						<div className='ml-1 transform transition flex items-center'>
 							{/* <BsChevronUp className='fill-gray-6 mx-1 fill-dark-gray-6' /> */}
@@ -15,7 +15,7 @@ const EditorFooter = ({ handleSubmit }) => {
 				</div>
 				<div className='ml-auto flex items-center space-x-4'>
 					<button
-						className='px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex bg-dark-fill-3  hover:bg-dark-fill-2 text-dark-label-2 rounded-lg'
+						className='px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex dark:bg-dark-fill-3 bg-gray-200 hover:bg-dark-fill-2 dark:text-dark-label-2 rounded-lg'
 						onClick={handleSubmit}
 					>
 						Run
--- app/components/Playground.js
+++ app/components/Playground.js
@@ -126,8 +126,8 @@ export default function Playground({problem}) {
                     {/* testcase heading */}
                     <div className='flex h-10 items-center space-x-6'>
                         <div className='relative flex h-full flex-col justify-center cursor-pointer'>
-                            <div className='text-sm font-medium leading-5 text-white'>Testcases</div>
-                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+                            <div className='text-sm font-medium leading-5 dark:text-white'>Testcases</div>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none dark:bg-white bg-black' />
                         </div>
                     </div>
 
@@ -140,8 +140,8 @@ export default function Playground({problem}) {
                             >
                                 <div className='flex flex-wrap items-center gap-y-4'>
                                     <div
-                                        className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
-										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+                                        className={`font-medium items-center transition-all focus:outline-none inline-flex dark:bg-dark-fill-3 bg-gray-200 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+										${activeTestCaseId === index ? "dark:text-white" : "text-gray-500"}
 									`}
                                     >
                                         Case {index + 1}
@@ -152,12 +152,12 @@ export default function Playground({problem}) {
                     </div>
 
                     <div className='font-semibold my-4'>
-                        <p className='text-sm font-medium mt-4 text-white'>Input:</p>
-                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                        <p className='text-sm font-medium mt-4 dark:text-white'>Input:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] dark:bg-dark-fill-3 bg-gray-200 border-transparent dark:text-white mt-2'>
                             {problem.examples[activeTestCaseId].inputText}
                         </div>
-                        <p className='text-sm font-medium mt-4 text-white'>Output:</p>
-                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                        <p className='text-sm font-medium mt-4 dark:text-white'>Output:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] dark:bg-dark-fill-3 bg-gray-200 border-transparent dark:text-white mt-2'>
                             {problem.examples[activeTestCaseId].outputText}
                         </div>
                     </div>
